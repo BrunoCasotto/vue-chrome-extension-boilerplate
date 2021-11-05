b21e46627dd87d8aa305a54e2d909e7c6da2a81f
Bruno Casotto <casottoalves@hotmail.com>
2021-11-05 09:19:03 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+
+const entry =  ['./src/main.js']
+
+const output = {
+  path: path.resolve(__dirname, './dist'),
+}
+
+const plugins = [
+  new VueLoaderPlugin(),
+  new HtmlWebpackPlugin({
+    template: './index.html'
+  }),
+  new GenerateJsonPlugin('manifest.json', {
+    name: "CsgoSeller",
+    description: "Tool to sell csgo items",
+    version: "1.0",
+    manifest_version: 3,
+    background: {
+      service_worker: "main.js"
+    }
+  })
+]
+
+const rules = [
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader',
+  },
+  {
+    test: /\.json$/,
+    loader: 'json-loader'
+  }
+]
+
+module.exports = {
+  entry,
+  output,
+  module: { rules },
+  plugins,
+}
